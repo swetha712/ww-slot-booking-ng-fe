e08d896f6f5365012c2882aadca10caa904d86f5
swetha712 <swethamurugan2222@gmail.com>
2024-11-27 10:14:00 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.exampletest.app',
+  appName: 'ww-slot-booking-app-fe',
+  webDir: 'dist/ww-slot-booking-app-fe/browser'
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatButtonModule} from '@angular/material/button'
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule,NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ww-slot-booking-app-fe';
+togglevar: boolean=false;
+toggle(){
+  this.togglevar=!this.togglevar;
+}
 }
