d752a13fa1c07df47c2ca1bc4e448b6d3cb785e8
Darwin Sakthivel <darwin.sakthivel@knacksystems.com>
2024-12-13 15:50:17 +0530

    routing config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,16 @@
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 // export function createTranslateLoader(http: HttpClient) {
 //   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 // }
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-
-  
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())]
+    provideHttpClient(withInterceptorsFromDi()),
+  provideAnimations()]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
