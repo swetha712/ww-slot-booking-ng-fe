8ce76e097a996557d4cd036ec0ead7fcd8a8f162
Darwin Sakthivel <darwin.sakthivel@knacksystems.com>
2024-12-16 11:48:03 +0530

    home page developement

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { UserAuthComponent } from './app/user-auth/user-auth.component';
+import { AppComponent } from './app/app.component';
 
 
-bootstrapApplication(UserAuthComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),// Enables animations
     provideRouter(routes),
